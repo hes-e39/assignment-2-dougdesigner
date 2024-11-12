12e97e4afc1075331ee929242f714069c449a3e8
Douglas Sanchez <38049028+dougdesigner@users.noreply.github.com>
2024-11-12 12:41:55 -0800

    Update round limit

--- src/components/generic/Input.tsx
+++ src/components/generic/Input.tsx
@@ -8,35 +8,28 @@ interface InputProps {
     disabled?: boolean;
 }
 
-const Input: React.FC<InputProps> = ({
-    minutes = 0,
-    seconds = 0,
-    rounds = 0,
-    onMinutesChange = () => {},
-    onSecondsChange = () => {},
-    onRoundsChange = () => {},
-    disabled = false,
-}) => {
+const Input: React.FC<InputProps> = ({ minutes = 0, seconds = 0, rounds = 0, onMinutesChange = () => {}, onSecondsChange = () => {}, onRoundsChange = () => {}, disabled = false }) => {
     const minuteOptions = Array.from({ length: 61 }, (_, i) => i);
     const secondOptions = Array.from({ length: 60 }, (_, i) => i);
-    const roundOptions = Array.from({ length: 10 }, (_, i) => i + 1);
+    const roundOptions = Array.from({ length: 30 }, (_, i) => i + 1);
 
     return (
         <div className="flex space-x-4 mt-8">
-
             {/* Minutes input */}
             <div className="flex flex-row-reverse items-center">
-                <label htmlFor="minutes" className="block text-lg font-semibold text-white">Minutes</label>
+                <label htmlFor="minutes" className="block text-lg font-semibold text-white">
+                    Minutes
+                </label>
                 <select
                     id="minutes"
                     value={minutes}
                     disabled={disabled}
-                    onChange={(e) => onMinutesChange(Number.parseInt(e.target.value))}
+                    onChange={e => onMinutesChange(Number.parseInt(e.target.value))}
                     className={`mr-2 py-2 px-4 block w-full bg-gray-800 border-2 border-gray-700 text-white rounded-lg shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-xl ${
                         disabled ? 'cursor-not-allowed opacity-50' : ''
                     }`}
                 >
-                    {minuteOptions.map((minute) => (
+                    {minuteOptions.map(minute => (
                         <option key={minute} value={minute}>
                             {minute.toString()}
                         </option>
@@ -46,17 +39,19 @@ const Input: React.FC<InputProps> = ({
 
             {/* Seconds input */}
             <div className="flex flex-row-reverse items-center">
-                <label htmlFor="seconds" className="block text-lg font-semibold text-white">Seconds</label>
+                <label htmlFor="seconds" className="block text-lg font-semibold text-white">
+                    Seconds
+                </label>
                 <select
                     id="seconds"
                     value={seconds}
                     disabled={disabled}
-                    onChange={(e) => onSecondsChange(Number.parseInt(e.target.value))}
+                    onChange={e => onSecondsChange(Number.parseInt(e.target.value))}
                     className={`mr-2 py-2 px-4 block w-full bg-gray-800 border-2 border-gray-700 text-white rounded-lg shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-xl ${
                         disabled ? 'cursor-not-allowed opacity-50' : ''
                     }`}
                 >
-                    {secondOptions.map((second) => (
+                    {secondOptions.map(second => (
                         <option key={second} value={second}>
                             {second.toString()}
                         </option>
@@ -67,17 +62,19 @@ const Input: React.FC<InputProps> = ({
             {/* Rounds input */}
             {rounds > 0 && (
                 <div className="flex flex-row-reverse items-center">
-                    <label htmlFor="rounds" className="block text-lg font-semibold text-white">Rounds</label>
+                    <label htmlFor="rounds" className="block text-lg font-semibold text-white">
+                        Rounds
+                    </label>
                     <select
                         id="rounds"
                         value={rounds}
                         disabled={disabled}
-                        onChange={(e) => onRoundsChange(Number.parseInt(e.target.value))}
+                        onChange={e => onRoundsChange(Number.parseInt(e.target.value))}
                         className={`mr-2 py-2 px-4 block w-full bg-gray-800 border-2 border-gray-700 text-white rounded-lg shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-xl ${
                             disabled ? 'cursor-not-allowed opacity-50' : ''
                         }`}
                     >
-                        {roundOptions.map((round) => (
+                        {roundOptions.map(round => (
                             <option key={round} value={round}>
                                 {round.toString()}
                             </option>
@@ -85,8 +82,6 @@ const Input: React.FC<InputProps> = ({
                     </select>
                 </div>
             )}
-
-
         </div>
     );
 };
--- src/components/generic/TabataInput.tsx
+++ src/components/generic/TabataInput.tsx
@@ -27,7 +27,7 @@ const TabataInput: React.FC<TabataInputProps> = ({
 }) => {
     const minuteOptions = Array.from({ length: 61 }, (_, i) => i);
     const secondOptions = Array.from({ length: 60 }, (_, i) => i);
-    const roundOptions = Array.from({ length: 10 }, (_, i) => i + 1);
+    const roundOptions = Array.from({ length: 30 }, (_, i) => i + 1);
 
     return (
         <div className="flex flex-col space-y-4 items-center mt-8">
@@ -37,14 +37,16 @@ const TabataInput: React.FC<TabataInputProps> = ({
                     <label className="text-lg font-semibold text-white">Minutes</label>
                     <select
                         value={workMinutes}
-                        onChange={(e) => onWorkMinutesChange(Number(e.target.value))}
+                        onChange={e => onWorkMinutesChange(Number(e.target.value))}
                         disabled={disabled}
                         className={`mr-2 py-2 px-4 bg-gray-800 border-2 border-gray-700 text-white rounded-lg shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-xl ${
                             disabled ? 'cursor-not-allowed opacity-50' : ''
                         }`}
                     >
-                        {minuteOptions.map((minute) => (
-                            <option key={minute} value={minute}>{minute}</option>
+                        {minuteOptions.map(minute => (
+                            <option key={minute} value={minute}>
+                                {minute}
+                            </option>
                         ))}
                     </select>
                 </div>
@@ -53,19 +55,20 @@ const TabataInput: React.FC<TabataInputProps> = ({
                     <label className="text-lg font-semibold text-white">Seconds</label>
                     <select
                         value={workSeconds}
-                        onChange={(e) => onWorkSecondsChange(Number(e.target.value))}
+                        onChange={e => onWorkSecondsChange(Number(e.target.value))}
                         disabled={disabled}
                         className={`mr-2 py-2 px-4 bg-gray-800 border-2 border-gray-700 text-white rounded-lg shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-xl ${
                             disabled ? 'cursor-not-allowed opacity-50' : ''
                         }`}
                     >
-                        {secondOptions.map((second) => (
-                            <option key={second} value={second}>{second}</option>
+                        {secondOptions.map(second => (
+                            <option key={second} value={second}>
+                                {second}
+                            </option>
                         ))}
                     </select>
                 </div>
             </div>
-
 
             <div className="flex space-x-4">
                 <h1 className="text-lg font-semibold text-white self-center">Rest:</h1>
@@ -73,30 +76,34 @@ const TabataInput: React.FC<TabataInputProps> = ({
                     <label className="text-lg font-semibold text-white">Minutes</label>
                     <select
                         value={restMinutes}
-                        onChange={(e) => onRestMinutesChange(Number(e.target.value))}
+                        onChange={e => onRestMinutesChange(Number(e.target.value))}
                         disabled={disabled}
                         className={`mr-2 py-2 px-4 bg-gray-800 border-2 border-gray-700 text-white rounded-lg shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-xl ${
                             disabled ? 'cursor-not-allowed opacity-50' : ''
                         }`}
                     >
-                        {minuteOptions.map((minute) => (
-                            <option key={minute} value={minute}>{minute}</option>
+                        {minuteOptions.map(minute => (
+                            <option key={minute} value={minute}>
+                                {minute}
+                            </option>
                         ))}
                     </select>
                 </div>
-                
+
                 <div className="flex flex-row-reverse items-center">
                     <label className="text-lg font-semibold text-white">Seconds</label>
                     <select
                         value={restSeconds}
-                        onChange={(e) => onRestSecondsChange(Number(e.target.value))}
+                        onChange={e => onRestSecondsChange(Number(e.target.value))}
                         disabled={disabled}
                         className={`mr-2 py-2 px-4 bg-gray-800 border-2 border-gray-700 text-white rounded-lg shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-xl ${
                             disabled ? 'cursor-not-allowed opacity-50' : ''
                         }`}
                     >
-                        {secondOptions.map((second) => (
-                            <option key={second} value={second}>{second}</option>
+                        {secondOptions.map(second => (
+                            <option key={second} value={second}>
+                                {second}
+                            </option>
                         ))}
                     </select>
                 </div>
@@ -107,14 +114,16 @@ const TabataInput: React.FC<TabataInputProps> = ({
                     <label className="text-lg font-semibold text-white">Rounds</label>
                     <select
                         value={rounds}
-                        onChange={(e) => onRoundsChange(Number(e.target.value))}
+                        onChange={e => onRoundsChange(Number(e.target.value))}
                         disabled={disabled}
                         className={`mr-2 py-2 px-4 bg-gray-800 border-2 border-gray-700 text-white rounded-lg shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-xl ${
                             disabled ? 'cursor-not-allowed opacity-50' : ''
                         }`}
                     >
-                        {roundOptions.map((round) => (
-                            <option key={round} value={round}>{round}</option>
+                        {roundOptions.map(round => (
+                            <option key={round} value={round}>
+                                {round}
+                            </option>
                         ))}
                     </select>
                 </div>
