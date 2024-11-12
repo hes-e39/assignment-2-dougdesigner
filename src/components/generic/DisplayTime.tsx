f9800783a3676b5a671c687cff43ceb906abe69d
Douglas Sanchez <38049028+dougdesigner@users.noreply.github.com>
2024-11-12 12:38:46 -0800

    Update DisplayTime.tsx

--- src/components/generic/DisplayTime.tsx
+++ src/components/generic/DisplayTime.tsx
@@ -4,10 +4,10 @@ interface DisplayTimeProps {
     hundredths?: number;
 }
 
-const DisplayTime: React.FC<DisplayTimeProps> = ({ minutes = 0, seconds = 0, hundredths = 0 }) => {
-    // Ensure two digits for minutes, seconds, annd miliseconds
-    const formatTime = (value: number, digits = 2) => value.toString().padStart(digits, '0');
+// Ensure two digits for minutes, seconds, annd miliseconds
+const formatTime = (value: number, digits = 2) => value.toString().padStart(digits, '0');
 
+const DisplayTime: React.FC<DisplayTimeProps> = ({ minutes = 0, seconds = 0, hundredths = 0 }) => {
     return (
         <div className="text-white my-5 font-mono text-4xl p-4 bg-gray-900 rounded-lg">
             {formatTime(minutes)}:{formatTime(seconds)}.{formatTime(hundredths)}
